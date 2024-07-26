2b2745d6698d717a9aaa71ad46c03a4395f89d30
Sammy Shear <sammyshear1@gmail.com>
2024-07-26 11:43:33 -0400

    google adsense

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,29 +1,29 @@
 import type { CollectionEntry } from "astro:content";
 
 type Props = {
-  posts: CollectionEntry<"blog">[];
+	posts: CollectionEntry<"blog">[];
 };
 
 export default function Posts({ posts }: Props) {
-  return (
-    <div className="flex flex-col space-y-10 mt-5">
-      {posts.map((post: CollectionEntry<"blog">) => {
-        return (
-          <a
-            className="flex flex-col bg-ctp-surface1 p-4 rounded-md"
-            key={post.id}
-            href={`/posts/${post.slug}`}
-          >
-            <span className="text-2xl">{post.data.title}</span>
-            <span className="text-sm text-ctp-subtext0">
-              {post.data.author}
-            </span>
-            <span className="text-sm text-ctp-subtext0">
-              {post.data.publishedTime.toLocaleDateString()}
-            </span>
-          </a>
-        );
-      })}
-    </div>
-  );
+	return (
+		<div className="flex flex-col space-y-10 mt-5">
+			{posts.map((post: CollectionEntry<"blog">) => {
+				return (
+					<a
+						className="flex flex-col bg-ctp-surface1 p-4 rounded-md"
+						key={post.id}
+						href={`/posts/${post.slug}`}
+					>
+						<span className="text-2xl">{post.data.title}</span>
+						<span className="text-sm text-ctp-subtext0">
+							{post.data.author}
+						</span>
+						<span className="text-sm text-ctp-subtext0">
+							{post.data.publishedTime.toLocaleDateString()}
+						</span>
+					</a>
+				);
+			})}
+		</div>
+	);
 }
